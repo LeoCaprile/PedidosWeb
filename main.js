58e91f7c272a9699b8c2f7910f2a3c1c9df3f29f
Matias Fuentes <matiasf.cortez@gmail.com>
2021-04-05 23:43:56 -0400

    Arreglo de UI y desarrollo de consumo de API

--- main.js
+++ main.js
@@ -4,10 +4,33 @@ fetch('https://serverless-leocaprile.vercel.app/api/meals')
     const mealList = document.getElementById('meals-list');
     const submit = document.getElementById('btn-enviar');
 
-    const template = data.map(el => `<a>${el.name}</a>`).join(' ')
-    mealList.innerHTML = template;
+    const template = data.map(el => 
+    `<li><a>${el.name}<button class="button is-small desc">Informacion</button></a>
+    <ul class="is-hidden">
+        <li><a>Descripcion: ${el.desc}</a></li>
+        <li><a>ID: ${el._id}</a></li>
+    </ul>
+    
+    </li>`).join(' ')
 
+    mealList.innerHTML = template;
+    btnInfoAccion();
     submit.removeAttribute('disabled')
 })
 
 
+
+function btnInfoAccion(){
+
+    const infoBtn = document.querySelectorAll('.desc');
+
+    infoBtn.forEach(elem => {
+        elem.addEventListener('click', ()=>{
+            const li = elem.parentElement.parentElement;
+            const info = li.lastElementChild;
+    
+            info.classList.toggle('is-hidden')
+        })
+    })
+
+}
